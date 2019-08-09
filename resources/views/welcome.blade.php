04809005855f76aa0beebb84869f1f5a0e3cab0d
CMHayden <haydencallum4@gmail.com>
2019-08-09 15:45:36 +0200

    guzzle 6.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
         <!-- Styles -->
         <style>
             html, body {
@@ -40,14 +43,7 @@
                 top: 18px;
             }
 
-            .content {
-                text-align: center;
-                background-color: #f0f0f0;
-                padding: 120px 20px;
-                width: 900px;
-            }
-
-            .title {
+            .text-logo {
                 font-size: 84px;
             }
 
@@ -66,65 +62,78 @@
             }
 
             .content {
-                animation: fadeIn ease 3s;
-                -webkit-animation: fadeIn ease 3s;
-                -moz-animation: fadeIn ease 3s;
-                -o-animation: fadeIn ease 3s;
-                -ms-animation: fadeIn ease 3s;
-                }
+                text-align: center;
+                background-color: #f0f0f0;
+                padding: 120px 20px;
+                width: 900px;
+                border-radius: 20px;
 
+                animation: fadeIn ease 5s;
+                -webkit-animation: fadeIn ease 5s;
+                -moz-animation: fadeIn ease 5s;
+                -o-animation: fadeIn ease 5s;
+                -ms-animation: fadeIn ease 5s;
+            }
 
-                @keyframes fadeIn{
+            @keyframes fadeIn{
                 0% {
                     opacity:0;
                 }
                 100% {
                     opacity:1;
                 }
-                }
+            }
 
-                @-moz-keyframes fadeIn {
+            @-moz-keyframes fadeIn {
                 0% {
                     opacity:0;
                 }
                 100% {
                     opacity:1;
                 }
-                }
+            }
 
-                @-webkit-keyframes fadeIn {
+            @-webkit-keyframes fadeIn {
                 0% {
                     opacity:0;
                 }
                 100% {
                     opacity:1;
                 }
-                }
+            }
 
-                @-o-keyframes fadeIn {
+            @-o-keyframes fadeIn {
                 0% {
                     opacity:0;
                 }
                 100% {
                     opacity:1;
                 }
-                }
+            }
 
-                @-ms-keyframes fadeIn {
+            @-ms-keyframes fadeIn {
                 0% {
                     opacity:0;
                 }
                 100% {
                     opacity:1;
                 }
-                }
+            }
+
+            .wrap {
+                text-align: center;
+                width: 100%;
+                margin-top: 10px;
+            }
         </style>
+
+
     </head>
     <body>
         <div class="flex-center position-ref full-height">
 
             <div class="content">
-                <div class="title m-b-md">
+                <div class="text-logo m-b-md">
                     PM App
                 </div>
 
@@ -132,11 +141,51 @@
                 @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="wrap">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
                     @endauth
                 </div>
             </div>
