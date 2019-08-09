f897eab70f52f1cd595dc5eb4d8d9ef7bc91263c
CMHayden <haydencallum4@gmail.com>
2019-08-09 15:52:43 +0200

    update home view to include new components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Settings</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,12 +13,12 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    <api-key></api-key>
+                    <br>
                     <passport-clients></passport-clients>
                     <br>
-                    <passport-authorized-clients></passport-authorized-clients>
-                    <br>
                     <passport-personal-access-tokens></passport-personal-access-tokens>
+
                     <br>
                 </div>
             </div>
