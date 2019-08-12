360f1ee8ff9713467a406d940d3e0c00a8134223
CMHayden <haydencallum4@gmail.com>
2019-08-12 17:12:14 +0200

    update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,25 +4,19 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Settings</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <api-key></api-key>
-                    <br>
-                    <passport-clients></passport-clients>
-                    <br>
-                    <passport-personal-access-tokens></passport-personal-access-tokens>
-
-                    <br>
-                </div>
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <api-key></api-key>
+                <br>
+                <passport-clients></passport-clients>
+                <br>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+                <br>
             </div>
-        </div>
     </div>
 </div>
 @endsection
